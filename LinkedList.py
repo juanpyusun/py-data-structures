d09f7afad0896844ab6817e46dd0074ba987e8f9
juan.yusunguaira <juanpyusun@gmail.com>
2024-09-21 14:45:00 -0500

    finish LinkedList and Node Classes

--- LinkedList.py
+++ LinkedList.py
@@ -1,38 +1,247 @@
 class LinkedList:
     def __init__(self, value=None):
+        """
+        Initializes a new linked list. If a value is provided, it creates the head and tail nodes.
+
+        :param value: The initial value to be stored in the first node of the list (optional).
+        """
         if value is None:
-            self.head = None
-            self.tail = None
-            self.length = 0
+            self.__head = None
+            self.__tail = None
+            self.__length = 0
         else:
             new_node = Node(value)
-            self.head = new_node
-            self.tail = new_node
-            self.length = 1
+            self.__head = new_node
+            self.__tail = new_node
+            self.__length = 1
+
+    @property
+    def head(self):
+        """
+        Gets the head node of the linked list.
+
+        :return: The head node of the linked list.
+        """
+        return self.__head
+
+    @property
+    def tail(self):
+        """
+        Gets the tail node of the linked list.
+
+        :return: The tail node of the linked list.
+        """
+        return self.__tail
+
+    @property
+    def length(self):
+        """
+        Gets the number of nodes in the linked list.
+
+        :return: The number of nodes in the linked list.
+        """
+        return self.__length
 
     def append(self, value):
+        """
+        Adds a new node with the provided value to the end of the linked list.
+
+        :param value: The value to be added to the linked list.
+        :return: True if the operation was successful.
+        """
         new_node = Node(value)
 
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
+        if self.__head is None:
+            self.__head = new_node
+            self.__tail = new_node
         else:
-            self.tail.next = new_node
-            self.tail = new_node
-        self.length += 1
-        return True # Esta linea es para una funcion posterior
+            self.__tail.next = new_node
+            self.__tail = new_node
+        self.__length += 1
+        return True
 
-    
+    def get(self, index):
+        """
+        Retrieves the node at the specified index in the linked list.
+
+        :param index: The index of the node to retrieve.
+        :return: The node at the specified index, or None if the index is out of bounds.
+        """
+        if index < 0 or index >= self.__length:
+            return None
+
+        temp = self.__head
+        
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def set_value(self, index, value):
+        """
+        Sets the value of the node at the specified index.
+
+        :param index: The index of the node whose value is to be set.
+        :param value: The new value to be assigned to the node.
+        :return: True if the operation was successful, False otherwise.
+        """
+        temp = self.get(index)
+        
+        if temp:
+            temp.data = value
+            return True
+        
+        return False
+
+    def insert(self, index, value):
+        """
+        Inserts a new node with the provided value at the specified index.
+
+        :param index: The index at which to insert the new node.
+        :param value: The value to be added to the linked list.
+        :return: True if the operation was successful, False otherwise.
+        """
+        if index < 0 or index > self.__length:
+            return False
+
+        if index == 0:
+            return self.prepend(value)
+        
+        if index == self.__length:
+            return self.append(value)
+        
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.__length += 1
+        return True
+
+    def remove(self, index):
+        """
+        Removes the node at the specified index from the linked list.
+
+        :param index: The index of the node to remove.
+        :return: The removed node, or None if the index is out of bounds.
+        """
+        if index < 0 or index >= self.__length:
+            return None
+
+        if index == 0:
+            return self.pop_first()
+
+        if index == self.__length - 1:
+            return self.pop()
+        
+        prev = self.get(index - 1)
+        temp = prev.next  # Avoid using get() for O(1) access
+        prev.next = temp.next
+        temp.next = None
+        self.__length -= 1
+        return temp
+
+    def reverse(self):
+        """
+        Reverses the linked list in place.
+        """
+        temp = self.__head
+        self.__head, self.__tail = self.__tail, self.__head
+        
+        after = temp.next
+        before = None
+        for _ in range(self.__length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+    def pop(self):
+        """
+        Removes and returns the last node from the linked list.
+
+        :return: The removed node, or None if the list is empty.
+        """
+        if self.__length == 0:
+            return None
+
+        temp = self.__head
+        pre = self.__head
+
+        while temp.next:
+            pre = temp
+            temp = temp.next
+
+        self.__tail = pre
+        self.__tail.next = None
+        self.__length -= 1
+
+        if self.__length == 0:
+            self.__head = None
+            self.__tail = None  
+        
+        return temp
+
+    def prepend(self, value):
+        """
+        Adds a new node with the provided value to the beginning of the linked list.
+
+        :param value: The value to be added to the linked list.
+        :return: True if the operation was successful.
+        """
+        new_node = Node(value)
+
+        if self.__length == 0:
+            self.__head = new_node
+            self.__tail = new_node
+        else:
+            new_node.next = self.__head
+            self.__head = new_node
+        self.__length += 1
+        return True
+
+    def pop_first(self):
+        """
+        Removes and returns the first node from the linked list.
+
+        :return: The removed node, or None if the list is empty.
+        """
+        if self.__length == 0:
+            return None
+
+        temp = self.__head
+        self.__head = self.__head.next
+        temp.next = None
+        self.__length -= 1
+
+        if self.__length == 0:
+            self.__tail = None
+
+        return temp
+
     def __str__(self):
-        temp = self.head
+        """
+        Returns a string representation of the linked list.
+
+        :return: A string showing the values of the linked list nodes.
+        """
+        temp = self.__head
         string = ""
         while temp is not None:
-            string += f"{temp.value} -> "
+            string += f"{temp.data} -> "
             temp = temp.next
-        return string
-    
+        return string + "None"
+
     def __len__(self):
-        return self.length
-    
+        """
+        Returns the number of nodes in the linked list.
+
+        :return: The number of nodes in the linked list.
+        """
+        return self.__length
+
     def __repr__(self):
-        return f"head:{self.head}, tail:{self.tail}, length:{self.length}"
+        """
+        Returns a string representation for debugging purposes.
+
+        :return: A string showing the head, tail, and length of the linked list.
+        """
+        return f"LinkedList(head={self.__head}, tail={self.__tail}, length={self.__length})"
--- Node.py
+++ Node.py
@@ -1,14 +1,99 @@
+# Clase Nodo, cada elemento de la LinkedList
 class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
     
-    def __str__(self):
-        return f"{self.value}"
-    
+    def __init__(self, initdata):
+        """
+        Initializes a new node with the provided data and sets the next node to None.
+
+        :param initdata: The data to be stored in the node.
+        """
+        self.__data = initdata  # Using __data to indicate it's a "private" attribute
+        self.__next = None      # The next node is initialized as None
+
+    @property
+    def data(self):
+        """
+        Gets the data of the node.
+
+        :return: The data stored in the node.
+        """
+        return self.__data
+
+    @data.setter
+    def data(self, newdata):
+        """
+        Sets the data of the node to the provided new value.
+
+        :param newdata: The new value to be assigned to the node's data.
+        """
+        self.__data = newdata
+
+    @property
+    def next(self):
+        """
+        Gets the next node.
+
+        :return: The next node in the list.
+        """
+        return self.__next
+
+    @next.setter
+    def next(self, newnext):
+        """
+        Sets the next node to the provided node.
+
+        :param newnext: The node to be assigned as the next node.
+        """
+        self.__next = newnext
+
     def __repr__(self):
-        return f"value:{self.value}, next:{self.next}"
+        """
+        Returns a string representation of the node.
+
+        :return: A string showing the data of the node.
+        """
+        return f"Node(data={self.__data})"
+
+    def __str__(self):
+        """
+        Returns a human-readable string of the node.
+
+        :return: A representative string of the node.
+        """
+        return f"Node with data: {self.__data}"
+
+    def __eq__(self, other):
+        """
+        Compares this node with another node by their data.
+
+        :param other: The other node to compare.
+        :return: True if the nodes have the same data, False otherwise.
+        """
+        if isinstance(other, Node):
+            return self.__data == other.data
+        return NotImplemented
+
+    def __ne__(self, other):
+        """
+        Compares this node with another node for inequality based on their data.
+
+        :param other: The other node to compare.
+        :return: True if the nodes have different data, False otherwise.
+        """
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        """
+        Returns a hash of the node based on its data.
+
+        :return: A hash value representing the node's data.
+        """
+        return hash(self.__data)
 
     def __len__(self):
+        """
+        Returns the size of the node, which is constant.
+
+        :return: Always returns 1, as a node represents a single element.
+        """
         return 1
-  
