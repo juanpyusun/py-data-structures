b006ff265f5eb9152acb309757badbc6d23a8844
juan.yusunguaira <juanpyusun@gmail.com>
2024-09-27 14:25:29 -0500

    added minheap class

--- non_linear/Heap.py
+++ non_linear/Heap.py
@@ -1,7 +1,7 @@
 from typing import Optional
     
 # Clase Max-Heap (valor maximo en la raiz)
-class Heap:
+class MaxHeap:
     def __init__(self, value=None)->None:
         self.__heap = []
     
@@ -64,3 +64,61 @@ class Heap:
         return str(self.__heap) 
 
  
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent(self, index):
+        return (index - 1) // 2
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+
+        while current > 0 and self.heap[current] < self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
+
+    def _sink_down(self, index):
+        min_index = index
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+
+            if (left_index < len(self.heap) and
+                    self.heap[left_index] < self.heap[min_index]):
+                min_index = left_index
+
+            if (right_index < len(self.heap) and
+                    self.heap[right_index] < self.heap[min_index]):
+                min_index = right_index
+
+            if min_index != index:
+                self._swap(index, min_index)
+                index = min_index
+            else:
+                return
+    
+
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        min_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+
+        return min_value
